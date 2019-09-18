b5effe5b48fca34ac2804f6c494fb4a1734335fd
Curtis Allen <callen@slack-corp.com>
2019-09-18 08:39:03 -0600

    add build commit version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { App, subtype } = require('@slack/bolt');
+const tmpl = require('reverse-string-template');
 
 const port = process.env.PORT || 8080;
 const circleCIBotId = 'BNGFZ7JTS';
@@ -10,12 +11,20 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+function getBuildCommit(circleCIMessage) {
+  const match = new RegExp('https://github.com/curtisallen/promote-bot-dsw.*>');
+  const commit = match.exec(circleCIMessage)[0].slice(0, -1);
+  const gitCommit = commit.split('|');
+  return gitCommit[1];
+}
+
 app.message(subtype('bot_message'), ({ message, say }) => {
   if (message.bot_id !== circleCIBotId) {
     console.log("message isn't from circle ci ignoring");
     return;
   }
   if (message.attachments && message.attachments[0].text.startsWith('Success')) {
+    const buildCommit = getBuildCommit(message.attachments[0].text);
     // successful build lets ask to promote it
     say({
       blocks: [
@@ -23,7 +32,7 @@ app.message(subtype('bot_message'), ({ message, say }) => {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: ':pear: The tests have passed for build 123. Shall we deploy it?'
+            text: `:pear: The tests have passed for build \`${buildCommit}\` Shall we deploy it?`
           },
           accessory: {
             type: 'button',
