2fdfd8cdcc1d6c782ac6eed829ad14552e74f436
Curtis Allen <callen@slack-corp.com>
2019-09-18 08:06:28 -0600

    check for circle ci

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 
-const { App } = require('@slack/bolt');
+const { App, subtype } = require('@slack/bolt');
 
 const port = process.env.PORT || 8080;
+const circleCIBotId = 'BNGFZ7JTS';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.message(subtype('bot_message'), ({ message, say }) => {
+  if (message.bot_id !== circleCIBotId) {
+    console.log("message isn't from circle ci ignoring");
+    return;
+  }
+  if (message.attachments && message.attachments[0].text.startsWith('Success')) {
+    // successful build lets ask to promote it
+    say('do you wanna deploy?');
+  }
+});
 (async () => {
   await app.start(port);
   console.log(`⚡️ listening on :${port}`);
